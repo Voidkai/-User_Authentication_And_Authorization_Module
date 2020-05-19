8d231b96884260f3c7b7e06aebd2d0dcfc3f3bf9
Theo Sun <theo.sun@sap.com>
2020-05-19 16:42:44 +0800

    feat: init project

---
+++ src/main/groovy/corp/sap/internal/exp/ExpApplication.groovy
@@ -0,0 +1,13 @@
+package corp.sap.internal.exp
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ExpApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ExpApplication, args)
+	}
+
+}
