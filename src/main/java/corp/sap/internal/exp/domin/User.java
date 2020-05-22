2507e54511f2f674cd5b7acd96e4ca26b3671a70
I533777 <keith.wang@sap.com>
2020-05-22 16:36:21 +0800

    fix: switch to new branch

---
+++ src/main/java/corp/sap/internal/exp/domin/User.java
@@ -0,0 +1,63 @@
+package corp.sap.internal.exp.domin;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class User implements UserDetails {
+
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private Set<SimpleGrantedAuthority> permissions;
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
