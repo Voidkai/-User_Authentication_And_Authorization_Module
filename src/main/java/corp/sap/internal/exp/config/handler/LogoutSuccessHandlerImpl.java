b33f054c76b9f10986da5819a0fcc14f533c3039
I533777 <keith.wang@sap.com>
2020-05-27 14:58:09 +0800

    fix: update

--- src/main/java/corp/sap/internal/exp/config/handler/LogoutSuccessHandlerImpl.java
+++ src/main/java/corp/sap/internal/exp/config/handler/LogoutSuccessHandlerImpl.java
@@ -1,7 +1,6 @@
 package corp.sap.internal.exp.config.handler;
 
 import com.alibaba.fastjson.JSON;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import corp.sap.internal.exp.config.JsonResult;
 import corp.sap.internal.exp.config.ResultTool;
 import org.springframework.security.core.Authentication;
