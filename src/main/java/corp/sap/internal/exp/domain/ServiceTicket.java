323a0d33c09e25607a002b17300b86048cc6ca16
I533777 <keith.wang@sap.com>
2020-05-27 12:57:55 +0800

    feat: add ServiceTicket domain

---
+++ src/main/java/corp/sap/internal/exp/domain/ServiceTicket.java
@@ -0,0 +1,34 @@
+package corp.sap.internal.exp.domain;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class ServiceTicket implements Serializable {
+    private int id;
+    private Timestamp create_time;
+    private int user_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Timestamp getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Timestamp create_time) {
+        this.create_time = create_time;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+}
