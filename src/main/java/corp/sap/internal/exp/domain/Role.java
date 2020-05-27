26de537cfb8d4d09e573e66ede88bb0c803f0b7c
I533777 <keith.wang@sap.com>
2020-05-27 10:13:15 +0800

    fix:invert the domain folder

---
+++ src/main/java/corp/sap/internal/exp/domain/Role.java
@@ -0,0 +1,23 @@
+package corp.sap.internal.exp.domain;
+
+public class Role {
+
+    private int role_id;
+    private String name;
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
