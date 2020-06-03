9bf68582c85af58234b5ef65951ca8871ad3975d
I533777 <keith.wang@sap.com>
2020-06-03 16:21:25 +0800

    fix: update

--- src/main/java/corp/sap/internal/exp/controllers/v3/UserController.java
+++ src/main/java/corp/sap/internal/exp/controllers/v3/UserController.java
@@ -1,8 +1,5 @@
 package corp.sap.internal.exp.controllers.v3;
 
-import corp.sap.internal.exp.domain.User;
-import corp.sap.internal.exp.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
