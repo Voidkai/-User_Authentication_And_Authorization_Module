c31862435a5954dbfba43986c592467b9e76cc46
I533777 <keith.wang@sap.com>
2020-06-03 11:35:49 +0800

    fix: add @ActiveProfiles

--- src/test/java/corp/sap/internal/exp/controllers/v3/ServiceTicketControllerTest.java
+++ src/test/java/corp/sap/internal/exp/controllers/v3/ServiceTicketControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,6 +20,7 @@ import org.springframework.web.context.WebApplicationContext;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @WebAppConfiguration
+@ActiveProfiles("test")
 public class ServiceTicketControllerTest {
 
 	@Autowired
