ff351312922906fc8606bd0e12d1a6814bd50b0c
I533777 <keith.wang@sap.com>
2020-06-05 14:33:32 +0800

    fix: Prepate test data

--- src/test/java/corp/sap/internal/exp/controllers/v3/dataPrepare.java
+++ src/test/java/corp/sap/internal/exp/controllers/v3/dataPrepare.java
@@ -6,18 +6,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -35,24 +34,62 @@ public class dataPrepare {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).apply(springSecurity()) // apply spring security
                 .build();
     }
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     //@Ignore
     @Test
-    public void dataPrepare() throws Exception{
-        String[] username = new String[100];
-        String[] password = new String[100];
-        for(int i=0;i<100;i++){
-            username[i] = RandomStringUtils.randomAlphanumeric(10)+i;
-            password[i] = RandomStringUtils.randomAlphanumeric(10)+i;
+    public void UsersPrepare(){
+        int len = 10000;
+        try {
+            jdbcTemplate.update("delete from users");
+            String[] sql = new String[len];
+            sql[0] = "insert into users(user_id,username,password) values("+1+",\"admin\",\""+ passwordEncoder.encode("123456")+"\")";
+            sql[1] = "insert into users(user_id,username,password) values("+2+",\"wkx\",\""+ passwordEncoder.encode("123456")+"\")";
+            for(int i=2;i<len;i++){
+                sql[i] = "insert into users(user_id,username,password) values("+(i+1)+",\""+RandomStringUtils.randomAlphanumeric(10)+"\",\""+passwordEncoder.encode("123456")+"\")";
+            }
+            jdbcTemplate.batchUpdate(sql);
+        } catch (DataAccessException e) {
+            e.printStackTrace();
         }
+    }
 
-        for(int i=0;i<100;i++){
+    //@Ignore
+    @Test
+    public void ServiceTicketPrepare(){
+        int len = 10000;
+        try {
+            jdbcTemplate.update("delete from service_ticket");
+            String[] sql = new String[len];
+            sql[0] = "insert into service_ticket values ("+1+",CURRENT_TIMESTAMP," + 1 + ", \"" + "content"+1 + "\")";
+            sql[1] = "insert into service_ticket values ("+2+",CURRENT_TIMESTAMP," + 2 + ", \"" + "content"+2 + "\")";
+            for(int i=2;i<len;i++){
+                sql[i] ="insert into service_ticket values ("+(i+1)+",CURRENT_TIMESTAMP," + (i+1)+ ", \"" + "content"+(i+1) + "\")";
+            }
+            jdbcTemplate.batchUpdate(sql);
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+    }
 
-            String url1 = "/register?username="+ username[i]+"&password="+password[i];
-            mockMvc.perform(get(url1).with(httpBasic("admin", "123456")))
-                    .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print());
-            String url2 = "/api/v3/serviceTicket/addTicket?content=hello"+i;
-            mockMvc.perform(get(url2).with(httpBasic("admin", "123456")))
-                    .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print());
+    @Test
+    public void UserRolePrepare(){
+        int len = 10000;
+        try {
+            jdbcTemplate.update("delete from role_user");
+            String[] sql = new String[len];
+            sql[0] = "insert into role_user values ("+1+"," + 1 + ", " +1 + ")";
+            sql[1] = "insert into role_user values ("+2+"," + 2 + ","+2 +")";
+            for(int i=2;i<len;i++){
+                sql[i] ="insert into role_user values ("+(i+1)+"," + 2 + ","+(i+1) +")";
+            }
+            jdbcTemplate.batchUpdate(sql);
+        } catch (DataAccessException e) {
+            e.printStackTrace();
         }
     }
 }
