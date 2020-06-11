99228343babfc2cf329f3840fa08ff303f6f7bd4
I533777 <keith.wang@sap.com>
2020-06-11 14:51:56 +0800

    fix: fix test bug

--- src/test/java/corp/sap/internal/exp/ExpApplicationTests.java
+++ src/test/java/corp/sap/internal/exp/ExpApplicationTests.java
@@ -3,12 +3,14 @@ package corp.sap.internal.exp;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @WebAppConfiguration
+@ActiveProfiles("test")
 class ExpApplicationTests {
 
 	@Test
