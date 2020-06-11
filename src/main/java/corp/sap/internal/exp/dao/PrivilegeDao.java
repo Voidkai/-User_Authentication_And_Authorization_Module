db5819f8f4b9e39c378474de173907e01a043e56
I533777 <keith.wang@sap.com>
2020-06-11 12:50:26 +0800

    fix: delete comment block

--- src/main/java/corp/sap/internal/exp/dao/PrivilegeDao.java
+++ src/main/java/corp/sap/internal/exp/dao/PrivilegeDao.java
@@ -13,19 +13,6 @@ public class PrivilegeDao {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-//    public List<Map<String,Object>> getPrivByUser(Integer id){
-//        String sql = "SELECT DISTINCT p.* FROM users AS u " +
-//                "LEFT JOIN role_user AS ur ON    u.user_id = ur.user_id " +
-//                "LEFT JOIN roles AS r ON    r.role_id = ur.role_id " +
-//                "LEFT JOIN privilege_role AS rp ON    r.role_id = rp.role_id " +
-//                "LEFT JOIN privileges AS p ON    p.privilege_id = rp.privilege_id " +
-//                "WHERE    u.user_id = " + id;
-//
-//        List list = jdbcTemplate.queryForList(sql);
-//
-//        return list;
-//    }
-
     public List<Integer> getRoleIdByUser(Integer user_id){
         String sql = "select * FROM role_user WHERE user_id = "+ user_id;
 
