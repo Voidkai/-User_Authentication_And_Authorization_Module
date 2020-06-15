eda83c29a2d1414952b4e2d746ccb4248fd3b0c0
I533777 <keith.wang@sap.com>
2020-06-15 17:25:22 +0800

    fix: add test database script

---
+++ src/main/resources/resources/java_reference_test.sql
@@ -0,0 +1,95 @@
+CREATE DATABASE IF NOT EXISTS `java_reference_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `java_reference_test`;
+DROP TABLE IF EXISTS `privileges`;
+CREATE TABLE `privileges` (
+  `privilege_id` int(11) NOT NULL,
+  `privilege_code` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+TRUNCATE TABLE `privileges`;
+INSERT INTO `privileges` (`privilege_id`, `privilege_code`, `description`) VALUES
+(100, 'service_ticket_truncate', ''),
+(101, 'service_ticket_create', ''),
+(102, 'service_ticket_read', ''),
+(103, 'service_ticket_update', ''),
+(104, 'service_ticket_delete', '');
+DROP TABLE IF EXISTS `privilege_role`;
+CREATE TABLE `privilege_role` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `privilege_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+TRUNCATE TABLE `privilege_role`;
+INSERT INTO `privilege_role` (`id`, `role_id`, `privilege_id`) VALUES
+(1, 1, 100),
+(2, 1, 101),
+(3, 1, 102),
+(4, 1, 103),
+(5, 1, 104),
+(6, 2, 102),
+(7, 2, 103),
+(8, 3, 101),
+(9, 3, 102),
+(10,3, 103);
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
+  `role_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+TRUNCATE TABLE `roles`;
+INSERT INTO `roles` (`role_id`, `name`) VALUES
+(1, 'service_ticket_manager'),
+(2, 'service_ticket_processor'),
+(3, 'customer');
+DROP TABLE IF EXISTS `role_user`;
+CREATE TABLE `role_user` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+TRUNCATE TABLE `role_user`;
+INSERT INTO `role_user` (`id`, `role_id`, `user_id`) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 3, 3);
+DROP TABLE IF EXISTS `service_ticket`;
+CREATE TABLE `service_ticket` (
+  `id` int(11) NOT NULL,
+  `update_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `user_id` int(11) NOT NULL,
+  `content` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+TRUNCATE TABLE `service_ticket`;
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+TRUNCATE TABLE `users`;
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES
+(1, 'admin', '$2a$10$08.QZS7/nNO5Z25QRM1w6euQcYyYCalxdDTwlIGvJPUuwUguYGgj2'),
+(2, 'processor', '$2a$10$z/lkaBOXSe20Xhws.Vs88e4mYjgI1POaF9Tv.v8WsJdyMncDfVDv2'),
+(3, 'wkx', '$2a$10$z/lkaBOXSe20Xhws.Vs88e4mYjgI1POaF9Tv.v8WsJdyMncDfVDv2');
+ALTER TABLE `privileges`
+  ADD PRIMARY KEY (`privilege_id`);
+ALTER TABLE `privilege_role`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`role_id`);
+ALTER TABLE `role_user`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `service_ticket`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+ALTER TABLE `privilege_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+ALTER TABLE `roles`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `role_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `service_ticket`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
--- src/test/java/corp/sap/internal/exp/ExpApplicationTests.java
+++ src/test/java/corp/sap/internal/exp/ExpApplicationTests.java
@@ -1,15 +1,48 @@
 package corp.sap.internal.exp;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @WebAppConfiguration
 class ExpApplicationTests {
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Before
+	public void databasePrepare(){
+
+		ClassPathResource classPathResource = new ClassPathResource("resources/java_reference_test.sql");
+		try {
+			File file = classPathResource.getFile();
+			FileInputStream in = new FileInputStream(file);
+			int size = in.available();
+			byte[] buffer = new byte[size];
+			in.read(buffer);
+			in.close();
+			String str = new String(buffer,"UTF-8");
+			String[] strSplit=str.split(";");
+			jdbcTemplate.batchUpdate(strSplit);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Test
 	void contextLoads() {
